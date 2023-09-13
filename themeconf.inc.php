264503308014b39f8c61c0211bce4d0ff52df120
klaoun <36087587+klaoun@users.noreply.github.com>
2023-09-13 02:00:41 +0200

    themeconf

---
+++ themeconf.inc.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Theme Name: flector
+Version: auto
+Description: 3ème thème personnalisé de piwigo
+Theme URI:
+Author: klaoun
+Author URI:
+Has Settings: true
+*/
+$themeconf = array(
+  'name' => 'flector',
+  'parent' => 'default',
+  'icon_dir' => 'themes/flector/img',
+  'img_dir'      => 'themes/flector/img/',
+  'mime_icon_dir' => 'themes/flectotr/img/',
+  'activable' => 'true',
+  'load_parent_local_head' => 'false',
+  'load_parent_css' => 'false'
+);
+load_language('theme.lang', PHPWG_THEMES_PATH.'flector/');
+?>
